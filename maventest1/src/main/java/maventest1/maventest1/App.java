f8dc5a13894892d9605daa02b2215613c93e1507
Anusha Kilari <anusha.kilari@in.tesco.com>
2017-08-10 00:07:21 +0530

    adding project

---
+++ maventest1/src/main/java/maventest1/maventest1/App.java
@@ -0,0 +1,13 @@
+package maventest1.maventest1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
